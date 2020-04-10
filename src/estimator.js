3dfb9263900419a033fadcbece029f5878df4dbd
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-04-10 14:16:00 +0300

    refactored  challenge 1 1.6

--- src/estimator.js
+++ src/estimator.js
@@ -1,36 +1,38 @@
-const covid19ImpactEstimator = (data) => {
-  const { reportedCases, periodType, timeToElapse } = data;
-
-  function periodEstimator(period, time) {
-    switch (period) {
-      case 'weeks':
-        return Math.trunc((time * 7) / 3);
-      case 'months':
-        return Math.trunc((time * 30) / 3);
-      default:
-        return Math.trunc(time / 3);
-    }
+function periodEstimator(period, time) {
+  switch (period) {
+    case 'weeks':
+      return Math.trunc((time * 7) / 3);
+    case 'months':
+      return Math.trunc((time * 30) / 3);
+    default:
+      return Math.trunc(time / 3);
   }
+}
+
+const impactResults = ({ reportedCases, periodType, timeToElapse }) => {
+  const currentlyInfected = reportedCases * 10;
+
+  const infectionsByRequestedTime = currentlyInfected
+  * 2 ** periodEstimator(periodType, timeToElapse);
   return {
-    data,
-    impact: {
-      currentlyInfected: reportedCases * 10,
-      infectionsByRequestedTime() {
-        return (
-          this.currentlyInfected
-          * 2 ** periodEstimator(periodType, timeToElapse)
-        );
-      }
-    },
-    severeImpact: {
-      currentlyInfected: reportedCases * 50,
-      infectionsByRequestedTime() {
-        return (
-          this.currentlyInfected
-          * 2 ** periodEstimator(periodType, timeToElapse)
-        );
-      }
-    }
+    currentlyInfected,
+    infectionsByRequestedTime
   };
 };
+const severeResults = ({ reportedCases, periodType, timeToElapse }) => {
+  const currentlyInfected = reportedCases * 50;
+  const infectionsByRequestedTime = currentlyInfected
+  * 2 ** periodEstimator(periodType, timeToElapse);
+  return {
+    currentlyInfected,
+    infectionsByRequestedTime
+  };
+};
+
+const covid19ImpactEstimator = (data) => ({
+  data,
+  impact: impactResults,
+  severeImpact: severeResults
+});
+
 export default covid19ImpactEstimator;
