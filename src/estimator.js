66db5033303a620fb379724791f2fcbe8984d766
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-04-11 00:05:32 +0300

    challenge 4.2

--- src/estimator.js
+++ src/estimator.js
@@ -77,37 +77,5 @@ const covid19ImpactEstimator = (data) => ({
   severeImpact: severeImpact(data)
 });
 
-function getFromForm() {
-  const population = +document.querySelector('[data-population]').value;
 
-  const timeToElapse = +document.querySelector('[data-time-to-elapse]').value;
-  const reportedCases = +document.querySelector('[data-reported-cases]').value;
-  const totalHospitalBeds = +document.querySelector(
-    '[data-total-hospital-beds]'
-  ).value;
-
-  const periodType = document.querySelector('[data-period-type]').value;
-  const button = document.querySelector('[data-go-estimate]');
-
-  const inputData = {
-    region: {
-      name: 'Africa',
-      avgAge: 19.7,
-      avgDailyIncomeInUSD: 5,
-      avgDailyIncomePopulation: 0.71
-    },
-    periodType,
-    timeToElapse,
-    reportedCases,
-    population,
-    totalHospitalBeds
-  };
-
-  button.addEventListener('click', (e) => {
-    e.preventDefault();
-    covid19ImpactEstimator(inputData);
-  });
-}
-
-document.addEventListener('click', getFromForm);
 export default covid19ImpactEstimator;
