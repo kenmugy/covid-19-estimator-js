8cfc57d3859d2970aaf25c297d93e9c8aa84c87b
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-04-10 11:30:36 +0300

    challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,44 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const {
+    reportedCases,
+    periodType,
+    timeToElapse,
+    totalHospitalBeds,
+    region
+  } = data;
 
+  const periodEstimator = (period, currentlyInfected, time) => {
+    switch (period) {
+      case 'weeks':
+        return currentlyInfected * time * 2 ** 2;
+      case 'months':
+        return currentlyInfected * time * 2 ** 10;
+      default:
+        return currentlyInfected * time;
+    }
+  };
+  return {
+    data,
+    impact: {
+      currentlyInfected: reportedCases * 10,
+      infectionsByRequestedTime() {
+        return periodEstimator(
+          periodType,
+          this.currentlyInfected,
+          timeToElapse
+        );
+      }
+    },
+    severeImpact: {
+      currentlyInfected: reportedCases * 50,
+      infectionsByRequestedTime() {
+        return periodEstimator(
+          periodType,
+          this.currentlyInfected,
+          timeToElapse
+        );
+      }
+    }
+  };
+};
 export default covid19ImpactEstimator;
