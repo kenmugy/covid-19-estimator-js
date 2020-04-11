b2ad03f8ccf5015af263459462a8445a8226af23
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-04-11 11:05:47 +0300

    challenge 4.9.1

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
-// import covid19ImpactEstimator from './estimator';
+import covid19ImpactEstimator from './estimator';
 
-// const express = require('express');
+const express = require('express');
 
-// const app = express();
+const app = express();
 
-// app.use(express.json());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
-// //middleware
-// const middlefunc = (req,res,next) => {
-//     res.send(`${req.get('host')}${req.originalUrl}`)
-//     next()
-// }
+// middleware
+const middlefunc = (req, res, next) => {
+  res.send(`${req.get('host')}${req.originalUrl}`);
+  next();
+};
 
-// app.use(middlefunc)
+app.use(middlefunc);
 
-// const PORT = process.env.PORT || '4400';
-// const logs = []
+const PORT = process.env.PORT || '4400';
+const logs = [];
 
-// app.get('/api/v1/on-covid-19/logs', (req, res) => {
+app.get('/api/v1/on-covid-19/logs', (req, res) => {
 
-// });
-// app.post(
-//   '/api/v1/on-covid-19/:format',
-//   (req, res) => {
-//     if (req.params.format === 'xml') return null; // xml fromat
-//     res.json(covid19ImpactEstimator(req.body));
-//   }
-// );
+});
+app.post(
+  '/api/v1/on-covid-19/:format',
+  (req, res) => {
+    if (req.params.format === 'xml') return null; // xml fromat
+    res.json(covid19ImpactEstimator(req.body));
+  }
+);
 
-// app.listen(PORT, console.log(`listening on port ${PORT}`));
+app.listen(PORT, console.log(`listening on port ${PORT}`));
