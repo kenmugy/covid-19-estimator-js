385448dae4ebc0490c33bdf1c72f4e9cd877998b
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-04-11 09:43:34 +0300

    challenge 4.6

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// import covid19ImpactEstimator from './estimator';
+
+// const express = require('express');
+
+
+// const app = express();
+
+// app.use(express.json());
+
+
+// const PORT = process.env.PORT || '4400';
+// const logs = []
+
+// app.get('/api/v1/on-covid-19/logs', (req, res) => {
+
+// });
+// app.post(
+//   '/api/v1/on-covid-19/:format',
+//   (req, res) => {
+//     if (req.params.format === 'xml') return null; // xml fromat
+//     res.json(covid19ImpactEstimator(req.body));
+//   }
+// );
+
+// app.listen(PORT, console.log(`listening on port ${PORT}`));
